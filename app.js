b50b9765dd0bfde9217b66fa23c6a8ad6a061a80
CA <155806044+caayalaj@users.noreply.github.com>
2024-02-01 02:08:14 -0800

    ADD json to app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-        //index page change image on mouseover event
+        //index page change image on a mouseover event
         $(function ()
         
         { 
@@ -22,24 +22,61 @@
             $("img").height($("img").height()-100);
         });
 
+
+        //EMAIL - This happens after the button clikc event in the contact page
+        function submitForm() {
+            event.preventDefault();
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const message = document.getElementById('message').value;
+
+            //creats formData which is used in the next part    
+            const formData = {
+                name: name,
+                email: email,
+                message: message
+            };
+
+            
+            saveFormDataToJson(formData);
+            const formDataString = 'Name: ${name}\nEmail: ${email}\nMessage: ${message}';
+            //smtp info  smtp is an external web service that handles emails
+            Email.send({
+
+                SecureToken: "e839cdab-e29a-45ff-aec4-01b48e35c9a3",
+                To: 'cayacsproject@gmail.com',
+                From: "cesaraaj11@gmail.com",
+                Subject: "CS112 New Form",
+                Body: `${formDataString}`
+            })
+
+            //then will give info regarding if the program worked
+            .then(
+                () => {
+                    alert("Submitted successfully :)");
+                },
+                error => {
+                    alert("Try again :)");
+                    //used console to help test webapp
+                    console.error("email not sent :(", error);
+                }
+            );
+            event.preventDefault();
+        }
+        //creates object to save as JSON then creates it to be accessible in the website to the user
+
+function saveFormDataToJson(formData) {
+            
+            const jsonData = JSON.stringify(formData);
         
         
-        // let center = [34.006050, -118.256540]
-        // const map = tt.map({
-        //   key: "4S534k2DjfJLdwFBGNLhvdG02GVCHYG4",
-        //   container: "map",
-        //   center: center,
-        //   zoom:100
-        // })
-        // map.on('load',() => {
-        //   new tt.Marker().setLngLat(center).addTo(map)
-        // })
-      
+            const blob = new Blob([jsonData], { type: 'application/json' });
+            const a = document.createElement('a');
+            a.href = URL.createObjectURL(blob);
+            a.download = 'form_data.json';
+            a.click();
+        }
 
-        // var map = L.map('map').setView([(34.006050, -118.256540)], 13);
 
-        // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        //   attribution: '&copy; OpenStreetMap contributors'
-        // }).addTo(map);
-    
-        // var marker = L.marker([(34.006050, -118.256540)]).addTo(map);
+
+//Javascript was finnicky! apis and libraries r cool but they r a bit of a pain to work w as a beginner
